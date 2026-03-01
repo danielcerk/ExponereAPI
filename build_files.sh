7f0dae16ef9791ccd46f9696ecf33c2aa0d5edf6
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-01 12:41:30 -0300

    chore: add config files

---
+++ build_files.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 -m pip install --upgrade pip
+
+python3 -m pip install -r requirements.txt
+
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+python3 manage.py test --noinput --keepdb

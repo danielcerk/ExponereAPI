e630125b350236bbf36e9a32c474013519b0e2c6
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-09 09:31:14 -0300

    feat: added wishlist urls

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 urlpatterns = [
 
     path('auth/', include('api.auth.urls')),
-    path('catalogs/', include('catalog.urls')),
-    path('catalogs/', include('category.urls')),
-    path('catalogs/', include('product.urls')),
+    path('catalogs/', include('api.catalog.urls')),
+    path('catalogs/', include('api.category.urls')),
+    path('catalogs/', include('api.product.urls')),
+    path('catalogs/', include('api.wishlist.urls')),
+    path('catalogs/', include('api.keywords.urls')),
     path('panel/subscription/', include('api.subscription.urls')),
 
 ]

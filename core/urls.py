ab6373c0a26bd9160e38a9f9015fc1b56bf3dcc1
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-19 23:08:15 -0300

    feat: added api urls

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
+    path('api/v1/', include('api.urls')),
+
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 

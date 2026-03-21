3e8e701d33f802e74e20a6b293f04c951af40953
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-21 20:28:19 -0300

    feat: added new modules

---
+++ api/AI/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Vamos criar um prompt para gerar copies humanas, otimizadas para qualquer tipo de caso e fazer com que evite mostrar algo do tipo como "Aqui está uma descrição..."
+# Quando a API do GEMINI gerar uma copy , ele deverá salvar o modelo
+
+class Copy(models.Model):
+
+    pass
---
+++ api/trend_marketing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrendMarketingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api.trend_marketing'

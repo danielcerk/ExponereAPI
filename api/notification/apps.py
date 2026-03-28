990148896267e9b3b0b3247971c97e010d01d5b1
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-28 11:19:46 -0300

    feat: created notification with celery

--- api/notification/apps.py
+++ api/notification/apps.py
@@ -2,9 +2,6 @@ from django.apps import AppConfig
 
 
 class NotificationConfig(AppConfig):
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.notification'
-
-    def ready(self):
-        
-        import api.notification.signals
---
+++ api/notification/tasks.py
@@ -0,0 +1,73 @@
+from django.contrib.auth import get_user_model
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+from celery import shared_task
+
+User = get_user_model()
+
+MAILGUN_WELCOME_EMAIL_TEMPLATE = settings.MAILGUN_WELCOME_EMAIL_TEMPLATE
+
+@shared_task()
+def send_welcome_email_task(email, username):
+
+    message = EmailMessage(
+        subject="Bem-vindo à Exponere",
+        body="",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[email],
+    )
+
+    message.template_id = MAILGUN_WELCOME_EMAIL_TEMPLATE
+    message.merge_global_data = {
+        "name": username
+    }
+
+    try:
+
+        message.send()
+
+    except Exception as e:
+
+        print("Erro ao enviar email:", e)
+
+@shared_task()
+def send_password_reset_email_task(user, reset_url):
+
+    message = EmailMessage(
+        subject="Redefinição de senha - Exponere",
+        body="",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[user.email],
+    )
+
+    message.template_id = settings.MAILGUN_PASSWORD_RESET_EMAIL_TEMPLATE
+
+    message.merge_data = {
+        user.email: {
+            "name": user.first_name,
+            "reset_url": reset_url
+        }
+    }
+
+    message.send()
+
+@shared_task()
+def send_password_changed_email_task(user):
+
+    message = EmailMessage(
+        subject="Sua senha foi alterada - Exponere",
+        body="",
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[user.email],
+    )
+
+    message.template_id = settings.MAILGUN_CONFIRMATION_EMAIL_TEMPLATE
+
+    message.merge_data = {
+        user.email: {
+            "name": user.first_name
+        }
+    }
+
+    message.send()

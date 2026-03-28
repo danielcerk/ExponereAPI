6672c0755dd486cf1ba135e2b5affd35a323c09a
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-03-27 21:59:58 -0300

    feat: created blog

---
+++ api/blog/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from .utils import get_all_articles, get_article_by_slug
+
+class ArticlesSerializer(serializers.Serializer):
+
+    articles = serializers.SerializerMethodField()
+    pagination = serializers.SerializerMethodField()
+
+    def get_articles(self, obj):
+
+        sort = self.context.get("sort")
+        page = self.context.get("page", 1)
+
+        data = get_all_articles(sort=sort, page=page)
+
+        return data.get("articles", [])
+
+    def get_pagination(self, obj):
+
+        sort = self.context.get("sort")
+        page = self.context.get("page", 1)
+
+        data = get_all_articles(sort=sort, page=page)
+
+        return data.get("pagination", {})
+    
+class ArticleBySlugSerializer(serializers.Serializer):
+    
+    article = serializers.SerializerMethodField()
+
+    def get_article(self, obj):
+
+        slug = self.context.get("slug")
+
+        data = get_article_by_slug(slug)
+
+        if "error" in data:
+
+            return data
+
+        return data
+
--- api/blog/views.py
+++ api/blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import ArticleBySlugSerializer, ArticlesSerializer
+
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ArticlesView(APIView):
+    
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+
+        sort = request.query_params.get("sort")
+        page = request.query_params.get("page", 1)
+
+        serializer = ArticlesSerializer(
+
+            context={
+
+                "sort": sort,
+                "page": int(page)
+
+            }
+
+        )
+
+        return Response(serializer.data)
+
+class ArticleView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+
+        slug = self.kwargs.get("post_slug")
+
+        serializer = ArticleBySlugSerializer(
+
+            context={"slug": slug}
+
+        )
+
+        return Response(serializer.data)
+

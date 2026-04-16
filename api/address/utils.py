bfe3bc3ff0dd2456f6fd93088f8964f43f03e86f
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-04-15 21:50:11 -0300

    feat: created validator for cep

--- api/address/utils.py
+++ api/address/utils.py
@@ -1,5 +1,13 @@
 import requests
 
+from django.core.exceptions import ValidationError
+
+def validate_no_repeated_digits(value):
+
+    if value and len(set(value)) == 1:
+        
+        raise ValidationError('O CEP não pode conter todos os dígitos iguais.')
+
 def verify_cep(cep: str):
 
     url = f'https://viacep.com.br/ws/{cep}/json/'

8b841a63b5660e54f05dd44d2c9692647dcf89e8
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-04-15 22:05:28 -0300

    feat: added validator

--- api/utils.py
+++ api/utils.py
@@ -3,8 +3,65 @@ from django.core.exceptions import ValidationError
 
 def validate_no_repeated_chars(value):
 
-    cleaned = re.sub(r'\W', '', value)
+    cleaned = re.sub(r'\D', '', value)
 
-    if len(set(cleaned)) == 1:
+    if not cleaned:
 
-        raise ValidationError('Documento inválido: não pode conter caracteres repetidos.')
+        raise ValidationError('Documento inválido.')
+
+    if cleaned == cleaned[0] * len(cleaned):
+        raise ValidationError('Inválido: caracteres repetidos.')
+
+    most_common = max(set(cleaned), key=cleaned.count)
+
+    if cleaned.count(most_common) >= len(cleaned) - 1:
+
+        raise ValidationError('Inválido: padrão suspeito.')
+
+    if cleaned in '0123456789' or cleaned in '9876543210':
+
+        raise ValidationError('Inválido: sequência numérica.')
+
+    for size in range(1, len(cleaned)//2 + 1):
+
+        pattern = cleaned[:size]
+
+        if pattern * (len(cleaned)//size) == cleaned:
+
+            raise ValidationError('Inválido: padrão repetitivo.')
+
+    if len(cleaned) == 11:
+
+        for i in range(9, 11):
+
+            soma = sum(int(cleaned[num]) * ((i+1) - num) for num in range(i))
+            digito = ((soma * 10) % 11) % 10
+
+            if int(cleaned[i]) != digito:
+
+                raise ValidationError('CPF inválido.')
+
+        return cleaned
+
+    elif len(cleaned) == 14:
+
+        pesos_1 = [5,4,3,2,9,8,7,6,5,4,3,2]
+        pesos_2 = [6] + pesos_1
+
+        soma = sum(int(cleaned[i]) * pesos_1[i] for i in range(12))
+        dig1 = 11 - (soma % 11)
+        dig1 = dig1 if dig1 < 10 else 0
+
+        soma = sum(int(cleaned[i]) * pesos_2[i] for i in range(13))
+        dig2 = 11 - (soma % 11)
+        dig2 = dig2 if dig2 < 10 else 0
+
+        if int(cleaned[12]) != dig1 or int(cleaned[13]) != dig2:
+
+            raise ValidationError('CNPJ inválido.')
+
+        return cleaned
+
+    else:
+        
+        raise ValidationError('Documento deve ser CPF ou CNPJ válido.')

519159d45c288d5778d5b4fdd5704bf92288845a
Daniel Gomes <danielcerqueira7747@gmail.com>
2026-05-21 22:04:38 -0300

    feat: added marketing plugins

--- api/marketing/admin.py
+++ api/marketing/admin.py
@@ -1,3 +1,176 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+
+    TagManager,
+    MetaPixel,
+    GA4
+
+)
+
+@admin.register(TagManager)
+class TagManagerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "catalog",
+        "container_id",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "container_id",
+        "catalog__name",
+        "catalog__slug",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+        "author",
+    )
+
+    autocomplete_fields = (
+        "author",
+        "catalog",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    list_editable = ("is_active",)
+
+    fieldsets = (
+        ("Configuração GTM", {
+            "fields": (
+                "name",
+                "author",
+                "catalog",
+                "container_id",
+                "is_active",
+            )
+        }),
+        ("Datas", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
+
+
+@admin.register(MetaPixel)
+class MetaPixelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "catalog",
+        "pixel_id",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "pixel_id",
+        "catalog__name",
+        "catalog__slug",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+        "author",
+    )
+
+    autocomplete_fields = (
+        "author",
+        "catalog",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    list_editable = ("is_active",)
+
+    fieldsets = (
+        ("Configuração Meta Pixel", {
+            "fields": (
+                "name",
+                "author",
+                "catalog",
+                "pixel_id",
+                "is_active",
+            )
+        }),
+        ("Datas", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
+
+
+@admin.register(GA4)
+class GA4Admin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "catalog",
+        "measurement_id",
+        "property_id",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "measurement_id",
+        "property_id",
+        "catalog__name",
+        "catalog__slug",
+    )
+
+    list_filter = (
+        "is_active",
+        "created_at",
+        "author",
+    )
+
+    autocomplete_fields = (
+        "author",
+        "catalog",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    list_editable = ("is_active",)
+
+    fieldsets = (
+        ("Configuração Google Analytics 4", {
+            "fields": (
+                "name",
+                "author",
+                "catalog",
+                "measurement_id",
+                "property_id",
+                "is_active",
+            )
+        }),
+        ("Datas", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
--- api/marketing/apps.py
+++ api/marketing/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class MarketingConfig(AppConfig):
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.marketing'
+
+    def ready(self):
+        
+        import api.marketing.signals

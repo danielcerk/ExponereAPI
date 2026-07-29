dd652e48e18dc615c6dc0ebef535ee6be553511f
danielcerk <danielcerqueira2346@gmail.com>
2026-07-29 09:19:14 -0300

    feat: updated app

--- core/settings.py
+++ core/settings.py
@@ -268,7 +268,7 @@ if DEBUG:
             'rest_framework.throttling.UserRateThrottle'
         ],
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        'PAGE_SIZE': 1,
+        'PAGE_SIZE': 25,
     }
 
 else:

438f0818018feef1a570b8de97ad9e1cd5a6823e
danielcerk <danielcerqueira2346@gmail.com>
2026-08-06 22:08:03 -0300

    feat: added blog support

--- api/blog/serializers.py
+++ api/blog/serializers.py
@@ -2,43 +2,42 @@ from rest_framework import serializers
 
 from .utils import get_all_articles, get_article_by_slug
 
-class ArticlesSerializer(serializers.Serializer):
+from .models import Post
 
-    articles = serializers.SerializerMethodField()
-    pagination = serializers.SerializerMethodField()
+class ArticlesSerializer(serializers.ModelSerializer):
 
-    def _get_data(self):
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "subtitle",
+            "slug",
+            "image",
+            "content",
+            "author",
+            "is_published",
+            "published_at",
+            "created_at",
+            "updated_at",
+        ]
 
-        if not hasattr(self, "_cached_data"):
 
-            sort = self.context.get("sort")
-            page = self.context.get("page", 1)
+class ArticleBySlugSerializer(serializers.ModelSerializer):
 
-            self._cached_data = get_all_articles(page=page)
-
-        return self._cached_data
-
-    def get_articles(self, obj):
-
-        return self._get_data().get("articles", [])
-
-    def get_pagination(self, obj):
-        
-        return self._get_data().get("pagination", {})
-    
-class ArticleBySlugSerializer(serializers.Serializer):
-    
-    article = serializers.SerializerMethodField()
-
-    def get_article(self, obj):
-
-        slug = self.context.get("slug")
-
-        data = get_article_by_slug(slug)
-
-        if "error" in data:
-
-            return data
-
-        return data
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "subtitle",
+            "slug",
+            "image",
+            "content",
+            "author",
+            "is_published",
+            "published_at",
+            "created_at",
+            "updated_at",
+        ]
 
